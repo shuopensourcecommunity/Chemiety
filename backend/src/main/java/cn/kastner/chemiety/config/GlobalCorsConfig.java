6eb5ba5a0c335b559d4f68c17e01702af80e270b
kastnerorz <kastnerorz@gmail.com>
2018-04-27 10:30:24 +0800

    config

--- backend/src/main/java/cn/kastner/chemiety/config/GlobalCorsConfig.java
+++ backend/src/main/java/cn/kastner/chemiety/config/GlobalCorsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class GlobalCorsConfig {
@@ -15,8 +16,13 @@ public class GlobalCorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowCredentials(true)
+                    .allowedMethods("GET", "POST", "DELETE", "PUT")
+                    .maxAge(3600);
             }
         };
     }
 }
+

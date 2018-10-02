c3309c44f3ad4fee17a237f5a7caaabc83000ff5
kastnerorz <kastnerorz@gmail.com>
2018-10-02 22:08:08 +0800

    add prefix

--- backend/src/main/java/cn/kastner/chemiety/controller/FileController.java
+++ backend/src/main/java/cn/kastner/chemiety/controller/FileController.java
@@ -16,6 +16,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/api/v1")
 public class FileController {
 
 
--- backend/src/main/java/cn/kastner/chemiety/controller/PostController.java
+++ backend/src/main/java/cn/kastner/chemiety/controller/PostController.java
@@ -21,6 +21,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/api/v1")
 public class PostController {
 
     @InitBinder
